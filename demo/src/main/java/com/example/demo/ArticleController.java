dc1de7fe91032ff00b307c33b4d5521557bff176
liuying09 <judgment0903@gmail.com>
2023-01-05 16:36:52 +0800

    文章新增與撈全部文章

--- demo/src/main/java/com/example/demo/ArticleController.java
+++ demo/src/main/java/com/example/demo/ArticleController.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,9 +21,25 @@ public class ArticleController {
 	@Autowired
 	private ArticleRepository articleRepository;
 	
+	@RequestMapping("/findArticle")
+	public List<ArticleModel> findArticle(){
+		
+		List<ArticleModel> list = new ArrayList<ArticleModel>();
+		list = articleRepository.findAll();
+		
+		return list;
+	
+	}
 	
 	@RequestMapping("/addArticle")
-	public void saveArticle(@RequestBody ArticleModel articleModel,@RequestParam(name="fileField",required = false) MultipartFile fileField) throws IOException {
+//	public void saveArticle(@ModelAttribute ArticleModel articleModel,@RequestParam(name="articleImg",required = false) MultipartFile fileField) throws IOException {
+	public void saveArticle(@RequestParam(name="value",required = false)String valueEN ,
+			@RequestParam(name="title",required = false)String articleTitle
+			,@RequestParam(name="content",required = false)String articleContent
+			,@RequestParam(name="remark",required = false)String articleRemark
+			,@RequestParam(name="status",required = false)String articleStatus
+			,@RequestParam(name="file-to-upload",required = false) MultipartFile fileField) throws IOException {
+		
 		
 //		ArticleModel aModel = new ArticleModel();
 //		aModel.setValueEN(articleModel.getValueEN());
@@ -29,17 +48,31 @@ public class ArticleController {
 //		aModel.setArticleContent(articleModel.getArticleContent());
 //		aModel.setArticleRemark(articleModel.getArticleRemark());
 		
+		System.out.println("articleModel.getValueEN()= "+valueEN);
+		System.out.println("articleModel.getArticleTitle()= "+articleTitle);
+		System.out.println("articleModel.getArticleContent()= "+articleContent);
+		System.out.println("articleModel.getArticleRemark()= "+articleRemark);
+		System.out.println("articleModel.getArticleStatus()= "+articleStatus);
+		
+//		System.out.println("articleModel.getValueEN()= "+articleModel.getValueEN());
+//		System.out.println("articleModel.getArticleTitle()= "+articleModel.getArticleTitle());
+//		System.out.println("articleModel.getArticleContent()= "+articleModel.getArticleContent());
+//		System.out.println("articleModel.getArticleRemark()= "+articleModel.getArticleRemark());
+//		System.out.println("articleModel.getArticleStatus()= "+articleModel.getArticleStatus());
+		
 		byte[] image;
 
 		if(fileField == null) {
+			System.out.println("111");
 			image=null;
 		}else {
+			System.out.println("222");
 			image=fileField.getBytes();
 		}
 		
-//		articleRepository.save(aModel);
-		articleRepository.addArticle(articleModel.getValueEN(), image, articleModel.getArticleTitle(), 
-				articleModel.getArticleContent(), articleModel.getArticleRemark(), articleModel.getArticleStatus(), null, null);
+//		articleRepository.addArticle(articleModel.getValueEN(), image, articleModel.getArticleTitle(), 
+//				articleModel.getArticleContent(), articleModel.getArticleRemark(), articleModel.getArticleStatus(), null, null);
+		articleRepository.addArticle(valueEN,image,articleTitle,articleContent,articleRemark,articleStatus,null, null);
 	}
 	
 	
--- demo/src/main/java/com/example/demo/dao/ArticleRepository.java
+++ demo/src/main/java/com/example/demo/dao/ArticleRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,6 +14,9 @@ import jakarta.transaction.Transactional;
 
 public interface ArticleRepository extends JpaRepository<ArticleModel,Long>{
 
+	
+	List<ArticleModel> findAll();
+	
 	
 	// 註記為事務交易
 		@Transactional
